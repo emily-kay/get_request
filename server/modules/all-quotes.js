5e9c8d7cccbc163ae6b1fd56fbca395107e9dcbf
Emily Kay <35386386+emily-kay@users.noreply.github.com>
2018-04-22 08:42:20 -0500

    Updating with Katrina's birthday quotes

--- server/modules/all-quotes.js
+++ server/modules/all-quotes.js
@@ -1,5 +1,13 @@
 const quotes_data = [
-    { quote: 'Thank you for being such a great friend', author: 'EmKay'}
+    {quote: 'Thank you for being such a great friend', author: 'EmKay'},
+    {quote:"Happy birthday, Katrina! I'm so glad you were born, as we say in Turkish. And I'm so glad we're friends!",author:"Melek"},
+    {quote:"I'm so glad you came into our lives! you bring so much to our lives!!", author:"Cody K Sommer"},
+    {quote:"I'm so glad we managed to meet that wonderful October night!!  You're a beautiful soul <3  *hugs*",author:"Andrea"},
+    {quote:"Happy birthday Katrina! You're a great friend and confidant, and I'm glad I've gotten to know you. God grant you many years!",author:"Jacob"},
+    {quote:"Katrina, thank you for always being there for me. I'm immensely lucky you're a part of my life.",author:"Addison"},
+    {quote:"yogirl. your spring ensamble is boss. also you are lovely. Happy 24th<3 ",author:"Caroline"},
+    {quote:"Your friendship is even more impressive than EmKay's nascent coding skills!",author:"Gabe"},
+    {quote:"You have a weird amount of knowledge about flags and I'm afraid to look at your browser history now. ",author:"EmKay"}
 ];
 
 module.exports = quotes_data;
